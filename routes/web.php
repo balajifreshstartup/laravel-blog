8eacc08e09d3e51b161e81135392b0da37abbdeb
“balajifreshstartup” <>
2023-09-19 09:17:33 +0530

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+// User related routes
+Route::get('/', [UserController::class,'showCorrectHomepage'])->name('login');
+Route::post('/register',[UserController::class,'register'])->middleware('guest');
+Route::post('/login',[UserController::class,'login'])->middleware('guest');
+Route::post('/logout',[UserController::class,'logout'])->middleware('mustBeLoggedIn');
+
+// Blog post related routs
+Route::get('/create-post',[PostController::class,'showCreateForm'])->middleware('mustBeLoggedIn');
+Route::post('/create-post',[PostController::class,'storeNewPost'])->middleware('mustBeLoggedIn');
+Route::get('/post/{post}',[PostController::class,'viewSinglePost'])->middleware('mustBeLoggedIn');
+
+// Profile related routes
+Route::get('/profile/{profileuser:username}', [UserController::class,'profile']);
